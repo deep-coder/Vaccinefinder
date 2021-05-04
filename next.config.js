c16bfdedfcf464c024d39e8ad52cdcb26a43d98b
deepak2.jain <deepak2.jain@ril.com>
2021-05-04 18:23:09 +0530

    Ignore typescript errors

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
     images: {
       domains: ['m.media-amazon.com'],
     },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
   }
